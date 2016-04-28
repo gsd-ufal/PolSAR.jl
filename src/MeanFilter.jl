a78670dcdc66ea8419f94ffef286efeb2966e95b
Rivo Sarmento <rivo.sarmento@gmail.com>
2016-04-28 14:41:59 -0300

    Renaming variables

--- src/MeanFilter.jl
+++ src/MeanFilter.jl
@@ -1,17 +1,17 @@
-function MeanFilter(img, width, height)
-	newImg = img
+function MeanFilter(image, width, height)
+	newImage = image
 	for (i in 2:(width-1))
 		for (j in 2:(height-1))
-			newImg[i,j,1] =  (img[i-1,j-1,1] + img[i,j-1,1] 	+ img[i+1,j-1,1] + 
-							  img[i-1,j,1] 	 + img[i,j,1] 		+ img[i+1,j,1] +
-							  img[i-1,j+1,1] + img[i,j+1,1] 	+ img[i+1,j+1,1])/9
-			newImg[i,j,2] =  (img[i-1,j-1,2] + img[i,j-1,2] 	+ img[i+1,j-1,2] + 
-							  img[i-1,j,2] 	 + img[i,j,2] 		+ img[i+1,j,2] +
-							  img[i-1,j+1,2] + img[i,j+1,2] 	+ img[i+1,j+1,2])/9
-			newImg[i,j,3] =  (img[i-1,j-1,3] + img[i,j-1,3] 	+ img[i+1,j-1,3] + 
-							  img[i-1,j,3] 	 + img[i,j,3] 		+ img[i+1,j,3] +
-							  img[i-1,j+1,3] + img[i,j+1,3] 	+ img[i+1,j+1,3])/9
+			newImage[i,j,1] =  (image[i-1,j-1,1] + image[i,j-1,1] 	+ image[i+1,j-1,1] + 
+							  image[i-1,j,1] 	 + image[i,j,1] 		+ image[i+1,j,1] +
+							  image[i-1,j+1,1] + image[i,j+1,1] 	+ image[i+1,j+1,1])/9
+			newImage[i,j,2] =  (image[i-1,j-1,2] + image[i,j-1,2] 	+ image[i+1,j-1,2] + 
+							  image[i-1,j,2] 	 + image[i,j,2] 		+ image[i+1,j,2] +
+							  image[i-1,j+1,2] + image[i,j+1,2] 	+ image[i+1,j+1,2])/9
+			newImage[i,j,3] =  (image[i-1,j-1,3] + image[i,j-1,3] 	+ image[i+1,j-1,3] + 
+							  image[i-1,j,3] 	 + image[i,j,3] 		+ image[i+1,j,3] +
+							  image[i-1,j+1,3] + image[i,j+1,3] 	+ image[i+1,j+1,3])/9
 		end
 	end
-	return (newImg)
+	return (newImage)
 end
