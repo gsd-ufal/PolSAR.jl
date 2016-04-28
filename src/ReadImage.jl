399959dbb401960a58f6e8b318808a2d17b65c61
Rivo Sarmento <rivo.sarmento@gmail.com>
2016-04-28 14:41:32 -0300

    Renaming variables

--- src/ReadImage.jl
+++ src/ReadImage.jl
@@ -1,18 +1,17 @@
-function ReadImage(start, windowHeight, windowWidth, zoomHeight, zoomWidth, sourceHeight, sourceWidth, connection1, connection2, connection3)
+function ReadImage(roiStart, roiHeight, roiWidth, summaryHeight, summaryWidth, datasetHeight, datasetWidth, imageID1, imageID2, imageID3)
 	
 	println("Reading image...")    
 	tic()
-    A = ZoomImage(start, windowHeight, windowWidth, zoomHeight, zoomWidth, sourceHeight, sourceWidth, connection1)
-    B = ZoomImage(start, windowHeight, windowWidth, zoomHeight, zoomWidth, sourceHeight, sourceWidth, connection2)
-    C = ZoomImage(start, windowHeight, windowWidth, zoomHeight, zoomWidth, sourceHeight, sourceWidth, connection3)
+    A = summaryImage(roiStart, roiHeight, roiWidth, summaryHeight, summaryWidth, datasetHeight, datasetWidth, imageID1)
+    B = summaryImage(roiStart, roiHeight, roiWidth, summaryHeight, summaryWidth, datasetHeight, datasetWidth, imageID2)
+    C = summaryImage(roiStart, roiHeight, roiWidth, summaryHeight, summaryWidth, datasetHeight, datasetWidth, imageID3)
 	println("Time: ",toc())
 
 	println("Set list...")
     for i in 1:1000000
-        rpush(pipeline,connection1.name,A[i])
-        rpush(pipeline,connection2.name,B[i])
-        rpush(pipeline,connection3.name,C[i])
+        rpush(pipeline,imageID1.name,A[i])
+        rpush(pipeline,imageID2.name,B[i])
+        rpush(pipeline,imageID3.name,C[i])
     end
     
 end
-
